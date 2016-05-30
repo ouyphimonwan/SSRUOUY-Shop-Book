43f78a285f8e859ae16e259df02b34e68422b174
OUY <ouy.phimonwan@gmail.com>
2016-05-30 15:08:49 +0700

    Check

---
+++ app/src/main/java/appewtc/ouy/ssrushopbook/MyAlert.java
@@ -0,0 +1,31 @@
+package appewtc.ouy.ssrushopbook;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+/**
+ * Created by Pc on 30/5/2559.
+ */
+public class MyAlert {
+
+    public void myDialog(Context context,
+                         String strTitle,
+                         String strMessage) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setIcon(R.drawable.icon_question);
+        builder.setCancelable(false);
+        builder.setTitle(strTitle);
+        builder.setMessage(strMessage);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                dialogInterface.dismiss();
+
+            }
+        });
+        builder.show();
+
+    }
+
+}   // MyAlert Class
--- app/src/main/java/appewtc/ouy/ssrushopbook/SingUpActivity.java
+++ app/src/main/java/appewtc/ouy/ssrushopbook/SingUpActivity.java
@@ -2,12 +2,51 @@ package appewtc.ouy.ssrushopbook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class SingUpActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText nameEditText, surnameEditText,
+    useEditText, passwordEditText;
+    private String nameString, surnameString, userString, passwordString;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sing_up);
-    }
-}
+
+        //Bind Widget
+        nameEditText = (EditText) findViewById(R.id.editText);
+        surnameEditText = (EditText) findViewById(R.id.editText2);
+        useEditText = (EditText) findViewById(R.id.editText3);
+        passwordEditText = (EditText) findViewById(R.id.editText4);
+
+
+    }// Main Method
+
+    public void cliclkSingUpSign(View view) {
+
+        nameString = nameEditText.getText().toString().trim();
+        surnameString = surnameEditText.getText().toString().trim();
+        userString = useEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        //Chack Space
+        if (nameString.equals("") || surnameString.equals("") ||
+                userString.equals("") || passwordString.equals("")) {
+            //Have Space
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog(this,"มีช่องว่าง","กรุณากรอกทุกช่องค่ะ");
+        } else {
+            //No Space
+
+        }
+
+
+    } //clickSign
+
+} //Main Class
