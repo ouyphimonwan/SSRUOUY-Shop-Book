932b6646b3e1841f7b99babeb351e38155c06c64
OUY <ouy.phimonwan@gmail.com>
2016-05-30 13:04:56 +0700

    start

---
+++ app/src/main/java/appewtc/ouy/ssrushopbook/MainActivity.java
@@ -0,0 +1,20 @@
+package appewtc.ouy.ssrushopbook;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }//Main Method
+
+    public void clickSignUpMain(View view) {
+        startActivity(new Intent(MainActivity.this, SingUpActivity.class));
+    }
+
+} //Main Class
---
+++ app/src/main/java/appewtc/ouy/ssrushopbook/SingUpActivity.java
@@ -0,0 +1,13 @@
+package appewtc.ouy.ssrushopbook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SingUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sing_up);
+    }
+}
